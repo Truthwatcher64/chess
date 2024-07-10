6c9342aeb0a17bbd94decc674397fc2807558e63
Alex B <49875526+Truthwatcher64@users.noreply.github.com>
2024-07-09 18:46:04 -0600

    Fixed the queen's positions

--- client/src/main/java/ui/ChessBoardDraw.java
+++ client/src/main/java/ui/ChessBoardDraw.java
@@ -32,7 +32,7 @@ public class ChessBoardDraw {
         //black
         if(color!=null && color.equalsIgnoreCase("black")) {
             for(int i=1; i<=8; i++){
-                for(int j=1; j<=8; j++){
+                for(int j=8; j>=1; j--){
                     chessPieces.add(board.getPiece(new ChessPosition(i, j)));
                 }
             }
@@ -45,7 +45,7 @@ public class ChessBoardDraw {
         //white or observer
         else{
             for(int i=8; i>=1; i--){
-                for(int j=1; j<=8; j++){
+                for(int j=8; j>=1; j--){
                     chessPieces.add(board.getPiece(new ChessPosition(i, j)));
                 }
             }
